378f02bcc7dd628ee44336cbd978852819b08163
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2017-07-25 16:04:36 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function setup () {
+    createCanvas(1000, 800);
+}
+
+function draw() {
+    noFill();
+    strokeWeight(6.5);
+    ellipse(160, 150, 100, 100)
+    ellipse(220, 200, 100, 100)
+    ellipse(280, 150, 100, 100)
+    ellipse(340, 200, 100, 100)
+    ellipse(400, 150, 100, 100)
+    
+}
